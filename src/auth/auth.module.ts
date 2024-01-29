645ca7e0ff0277c0c279d76e584108478e8c3ec4
victor <joaovicif2021@gmail.com>
2024-01-29 15:41:05 -0300

    feita configuração de variaveis de ambiente

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,17 @@ import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import AuthGuard from './auth.guard';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '600s' },
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '600s' },
+      }),
     }),
   ],
   controllers: [AuthController],
